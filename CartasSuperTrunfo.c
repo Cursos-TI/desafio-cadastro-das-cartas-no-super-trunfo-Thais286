077a7aba35f71096bd88f6b4c6c388f1ed4add75
Thais286 <thaisamandaestacio@gmail.com>
2025-03-15 01:44:56 +0000

    git hub atualizacao

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -5,14 +5,14 @@ int main(){
 char estado[2];
 //sigla do estado 
 
-char codigo[10];
+char codigo[9];
 //codigo da carta
 
 char nome[50];
 //nome da cidade
 
-int habitantes =20000;
-//população da cidade
+char populacao =200000;
+//populacao da cidade
 
 float area =10000000;
 //area da cidade
@@ -25,33 +25,25 @@ int pontos =50;
 
 printf("qual o estado da carta?\n");
 scanf("%2s",estado);
-
 printf("qual o codigo da carta?\n");
-scanf("%10s",codigo);
-
+scanf("%9s",codigo);
 printf("qual o nome da cidade?\n");
-sacnf("%[^\n]",nome);
-//ler o nome com espaços
-
+scanf("%s",nome);
 printf("qual a populacao da cidade?\n");
-scanf("%d","&populacao");
-
+scanf("%f",populacao);
 printf("qual a area da cidade?\n");
-sacanf("%f",&area);
-
+sscanf("%f",&area);
 printf("qual o pib da cidade?\n");
-sacnf("%f",&pib);
-
+scanf("%2f",&pib);
 printf("quantos pontos turisticos essa cidade tem?\n");
 scanf("%d",&pontos);
 
-printf("dados da cidade\n")
-printf("estado:%s\n",A);
-printf("codigo da carta:%s\n",A01);
-printf("nome da cidade:%s\n","São Paulo");
-printf("populacao:%d 12325000 ");
-printf("area(em km²):%.2fM²\n",1521.11);
-printf("pib:%2fbilhoes\n" 699.28 bilhoes de reais);
+printf("estado:%s\n","A");
+printf("codigo da carta:%s\n","A01\n");
+printf("nome da cidade:%s\n","São Paulo\n");
+printf("populacao:%d 12325000\n");
+printf("area(em km²):%.2fM²\n","1521.11");
+printf("pib:%s","699.28 bilhoes de reais\n");
 printf("numero de pontos turisticos:%d\n",50);
 
 return 0;
