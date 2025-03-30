5899bda5cb30da01b3a8d5037c1f28b493fbc192
Thais286 <thaisamandaestacio@gmail.com>
2025-03-30 15:30:02 +0000

    dasafioSuperTriunfo

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,55 +1,96 @@
 #include <stdio.h>
-int main(){
-  printf("Desafiosuper-triunfo!\n");
 
-char estado[2];
-//sigla do estado 
+int main() {
 
-char codigo[9];
-//codigo da carta
+    // Declaração das variáveis
+    char estado;
+    char codigo[4]; // Ex: A01, B02
+    char nome[50];
+    int populacao;
+    float area;
+    float pib;
+    int pontos;
 
-char nome[50];
-//nome da cidade
+    // Entrada de dados
+    printf("Digite a letra do estado (A-H):\n ");
+    scanf(" %c", &estado);
 
-char populacao [50];
-//populacao da cidade
+    printf("Digite o código da carta (Ex: A01):\n ");
+    scanf("%3s", codigo);
 
-float area [50];
-//area da cidade
+    printf("Digite o nome da cidade:\n ");
+    scanf(" %49[^\n]", nome); // Permite espaços no nome
 
-float pib [50];
-//pib da cidade
+    printf("Digite a populacao da cidade:\n ");
+    scanf("%d", &populacao);
 
-int pontos [50];
-//pontos turisticos da cidade
+    printf("Digite a area da cidade (km²):\n ");
+    scanf("%f", &area);
 
-printf("qual o estado da carta?\n");
-scanf("%2s",estado);
-printf("qual o codigo da carta?\n");
-scanf("%9s",codigo);
-printf("qual o nome da cidade?\n");
-scanf("%s",nome);
-printf("qual a populacao da cidade?\n");
-scanf("%f",populacao);
-printf("qual a area da cidade?\n");
-sscanf("%f",&area);
-printf("qual o pib da cidade?\n");
-scanf("%2f",&pib);
-printf("quantos pontos turisticos essa cidade tem?\n");
-scanf("%d",&pontos);
+    printf("Digite o PIB da cidade (em bilhões):\n ");
+    while (getchar()!= '\n'); //limpa o buffer antes de scanf
+    scanf("%f", &pib);
 
-printf("estado:%s\n","A");
-printf("codigo da carta:%s\n","A01\n");
-printf("nome da cidade:%s\n","São Paulo\n");
-printf("populacao:%d 12325000\n");
-printf("area(em km²):%.2fM²\n","1521.11");
-printf("pib:%s","699.28 bilhoes de reais\n");
-printf("numero de pontos turisticos:%d\n",50);
+    printf("Digite o numero de pontos turísticos:\n ");
+    while (getchar()!= '\n'); //limpa o buffer antes de scanf
+    scanf("%d", &pontos);
 
-return 0;
+    // Exibição dos dados
+    printf("\n--- Dados da Carta1 ---\n");
+    printf("Estado: %c\n", estado);
+    printf("Código da Carta: %s\n", codigo);
+    printf("Nome da Cidade: %s\n", nome);
+    printf("População: %d habitantes\n", populacao);
+    printf("Área: %.2f km²\n", area);
+    printf("PIB: %.2f bilhões de reais\n", pib);
+    printf("Número de Pontos Turísticos: %d\n", pontos);
+
+
+    printf("\n---CARTA2---\n");
+    // Declaração das variáveis ,segunda carta
+    char estado2;
+    char codigo2[4]; // Ex: A01, B02
+    char nome2[50];
+    int populacao2;
+    float area2;
+    float pib2;
+    int pontos2;
+
+
+    // Entrada de dados
+    printf("Digite a letra do estado2 (A-H):\n ");
+    scanf(" %s", &estado2);
+
+    printf("Digite o código da carta2 (Ex: B02):\n ");
+    scanf("%3s", codigo2);
+
+    printf("Digite o nome da cidade2:\n ");
+    scanf(" %49s[^\n]", nome2); // Permite espaços no nome
+
+    printf("Digite a populacao da cidade2:\n ");
+    scanf("%d", &populacao2);
+
+    printf("Digite a area da cidade2 (km²):\n ");
+    scanf("%f", &area2);
+
+    printf("Digite o PIB da cidade2 (em bilhões):\n ");
+    while (getchar()!= '\n'); //limpa o buffer antes de scanf
+    scanf("%f", &pib2);
+
+    printf("Digite o numero de pontos2 turísticos:\n ");
+    while (getchar()!= '\n'); //limpa o buffer antes de scanf
+    scanf("%d", &pontos2);
+
+    // Exibição dos dados
+
+    printf("\n--- Dados da Carta1 ---\n");
+    printf("Estado: %c\n", estado);
+    printf("Código da Carta: %s\n", codigo);
+    printf("Nome da Cidade: %s\n", nome);
+    printf("População: %d habitantes\n", populacao);
+    printf("Área: %.2f km²\n", area);
+    printf("PIB: %.2f bilhões de reais\n", pib);
+    printf("Número de Pontos Turísticos: %d\n", pontos);
+
+    return 0;
 }
-
-
-
-
-
